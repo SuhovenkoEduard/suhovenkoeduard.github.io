fee8abec9eae8b220cb40efb924544430cc7d6b5
SuhovenkoEduard <73432765+SuhovenkoEduard@users.noreply.github.com>
2021-10-15 23:25:47 +0300

    1

--- Cubes/scripts/logic/providers/providers.js
+++
@@ -1,9 +0,0 @@
-import LocalProvider from "./fragments/local.js";
-import ServerProvider from "./fragments/server.js";
-
-const providers = {
-  local: LocalProvider,
-  server: ServerProvider
-};
-
-export default providers;
