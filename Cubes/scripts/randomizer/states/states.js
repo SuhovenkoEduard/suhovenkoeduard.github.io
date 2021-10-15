1bb59776d54e3075590538a342a984a3a8ccca59
SuhovenkoEduard <73432765+SuhovenkoEduard@users.noreply.github.com>
2021-10-15 23:27:16 +0300

    fragment

---
+++ Cubes/scripts/randomizer/states/states.js
@@ -0,0 +1,13 @@
+import Cancel from "./fragments/cancel.js";
+import Success from "./fragments/success.js";
+import Wait from "./fragments/wait.js";
+import Error from "./fragments/error.js";
+
+let states = {
+  Cancel,
+  Success,
+  Wait,
+  Error
+};
+
+export default states;
